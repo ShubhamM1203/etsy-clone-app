ad36c7a7eec650757743311f3f66368ef83b4578
ShubhamM1203 <96812228+ShubhamM1203@users.noreply.github.com>
2022-06-11 16:48:40 +0530

    Add files via upload

---
+++ src/Router/router.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { HashRouter, Route, NavLink, Switch } from 'react-router-dom';
+import SmartProductsScreen from '../Components/AllProductsScreen';
+import SmartCategoryScreen from '../Components/CategoryScreen';
+import SmartProductDetailScreen from '../Components/ProductDetailScreen'
+import About from '../Components/About'
+import SmartCart from '../Components/CartScreen'
+import ProductDetailScreen from  '../Components/ProductDetailScreen'
+
+let Router = () =>
+  <HashRouter>
+    <div>
+      <Switch>
+        <Route exact path='/home' component={SmartProductsScreen} />
+        <Route path='/cart' component={SmartCart}></Route>
+        <Route exact path='/about' component={About} />
+        <Route excact path='/product/:id' component={ProductDetailScreen} />
+        <Route exact path='/category/:slug' component={SmartCategoryScreen} />
+        <Route path='/product/:id' component={SmartProductDetailScreen} />
+      </Switch>
+    </div>
+  </HashRouter>
+
+export default Router;
